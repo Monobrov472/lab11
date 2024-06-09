2bd8bb02252f75f15c8d272a8cacf2f2c649db2e
Monobrov472 <144647756+Monobrov472@users.noreply.github.com>
2024-06-09 21:39:20 +0300

    Add files via upload

---
+++ lab11.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+struct Time {
+    int hours;
+    int minutes;
+    int seconds;
+};
+
+int secondsPassedSinceLastHour(int totalSeconds) {
+    int secondsInHour = 3600; // 1 година = 3600 секунд
+    int secondsPassed = totalSeconds % secondsInHour;
+    return secondsPassed;
+}
+
+int main() {
+    int N;
+    std::cout << "Enter the number of seconds that have passed since the start of the day: ";
+    std::cin >> N;
+
+    // Перетворення секунд у години, хвилини та секунди
+    int hours = N / 3600;
+    int remainingSeconds = N % 3600;
+    int minutes = remainingSeconds / 60;
+    int seconds = remainingSeconds % 60;
+
+    std::cout << "hours: " << hours << ", minutes: " << minutes << ", seconds: " << seconds << std::endl;
+
+    // Обчислення кількості секунд, що пройшли з початку останньої години
+    int secondsLastHour = secondsPassedSinceLastHour(N);
+    std::cout << "Seconds that have passed since the beginning of the last hour: " << secondsLastHour << std::endl;
+
+    return 0;
+}
